ca15e409fd4c047756d54c2d75350f9d5f1c25e5
Vladimir Zhavoronkov <vladimirz@airg.com>
2015-06-12 17:25:24 +0000

    Added POD

--- lib/JIP/Daemon.pm
+++ lib/JIP/Daemon.pm
@@ -220,3 +220,175 @@ sub _log {
 
 1;
 
+__END__
+
+=head1 NAME
+
+JIP::Daemon - Daemonize server process.
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+    use JIP::Daemon;
+    use Mojo::Log;
+
+    my $proc = JIP::Daemon->new(
+        logger  => Mojo::Log->new,
+        dry_run => 1,
+    );
+
+    $proc->daemonize;
+
+    if ($proc->try_kill(0) and $proc->detached) {
+        printf qq{pid(%s), active(%s), detached(%s)\n}, $proc->status;
+    }
+
+=head1 ATTRIBUTES
+
+L<JIP::Daemon> implements the following attributes.
+
+=head2 pid
+
+    my $pid = $proc->pid;
+
+=head2 detached
+
+    my $detached => $proc->detached;
+
+Process is detached from the controlling terminal.
+
+=head2 dry_run
+
+    my $dry_run = $proc->dry_run;
+
+Perform a trial run with no changes made (foreground if dry_run).
+
+=head2 uid
+
+    my $uid = $proc->uid;
+
+The real user identifier and the effective user identifier for this process.
+
+=head2 gid
+
+    my $gid = $proc->gid;
+
+The real group identifier and the effective group identifier for this process.
+
+=head2 cwd
+
+    my $cwd = $proc->cwd;
+
+Change the current working directory.
+
+=head2 umask
+
+    my $umask = $proc->umask;
+
+Most Daemon processes runs as super-user, for security reasons they should protect files that they create. Setting user mask will prevent unsecure file priviliges that may occur on file creation.
+
+=head2 logger
+
+    $proc->logger->info(q{Hello});
+
+Simple logger, based on L<Mojo::Log> interface.
+
+=head2 log_callback
+
+    my $log_callback = $proc->log_callback;
+
+=head1 METHODS
+
+=head2 new
+
+    my $proc = JIP::Daemon->new;
+    my $proc = JIP::Daemon->new(dry_run => 1);
+
+Construct a new L<JIP::Daemon> object.
+
+=head2 daemonize
+
+    $proc = $proc->daemonize;
+
+Daemonize server process.
+
+=head2 reopen_std
+
+    $proc = $proc->reopen_std;
+
+Reopen STDIN, STDOUT, STDERR to /dev/null.
+
+=head2 drop_privileges
+
+    my $proc = JIP::Daemon->new(uid => 1000, gid => 1000, umask => 0, cwd => q{/});
+    $proc = $proc->drop_privileges;
+
+Change uid/gid/umask/cwd on demand.
+
+=head2 try_kill
+
+    my $is_alive = $proc->try_kill(0);
+
+This is identical to Perl's builtin kill() function for sending signals to processes (often to terminate them).
+
+=head2 status
+
+    my ($pid, $is_alive, $is_detached) = $proc->status;
+
+Returns a list of process attributes: PID, is alive, detached (in backgroung).
+
+=head1 SEE ALSO
+
+POSIX, Privileges::Drop, Proc::Daemon
+
+=head1 AUTHOR
+
+Vladimir Zhavoronkov, C<< <flyweight at yandex.ru> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Vladimir Zhavoronkov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+
